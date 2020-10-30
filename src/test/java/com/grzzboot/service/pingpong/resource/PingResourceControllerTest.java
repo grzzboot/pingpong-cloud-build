b51a3d6ea20e13642cab54c995c30f80807f43fa
grzzboot <per.andreas.engstrom@gmail.com>
2020-10-30 17:21:26 +0100

    Failing JUnit test

---
+++ src/test/java/com/grzzboot/service/pingpong/resource/PingResourceControllerTest.java
@@ -0,0 +1,14 @@
+package com.grzzboot.service.pingpong.resource;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class PingResourceControllerTest {
+
+	@Test
+	public void failingTest() {
+		fail("Oh no!");
+	}
+	
+}
