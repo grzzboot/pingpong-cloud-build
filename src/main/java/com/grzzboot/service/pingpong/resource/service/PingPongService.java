baa04cee4f64c275e21a4b0de09a5be66b8cf668
grzzboot <per.andreas.engstrom@gmail.com>
2020-10-30 17:10:25 +0100

    Pushing a working change to a feature branch

--- src/main/java/com/grzzboot/service/pingpong/resource/service/PingPongService.java
+++ src/main/java/com/grzzboot/service/pingpong/resource/service/PingPongService.java
@@ -8,7 +8,7 @@ import com.grzzboot.service.pingpong.resource.service.domain.PingEntity;
 @Service
 public class PingPongService {
 
-	private static final String MESSAGE_BASE = "Pong";
+	private static final String MESSAGE_BASE = "Pong working feature branch";
 	
 	public PingEntity ping(String name) {
 		StringBuilder sb = new StringBuilder(MESSAGE_BASE);
